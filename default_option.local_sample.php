926479b27fd90708428101a2d603d8bc5cb904af
Rus1anLev <im.ruslan.lev@gmail.com>
2023-05-26 10:53:24 +0300

    removed remote dev_site dir from local options sample

--- default_option.local_sample.php
+++ default_option.local_sample.php
@@ -1,7 +1,6 @@
 <?
 
 $medialine_base_default_option = array(
-    "DEV_SITE_DIR" => "", // /home/bitrix',
     "DEV_DATABASE_NAME" => "", // "dev_db",
     "DEV_DATABASE_LOGIN" => "", // "dev_db_user",
     "DEV_DATABASE_PASSWORD" => "", //"'%Z3+%rWK&[YyvF=iSfSR'", Если у пароля есть спец символы — обязательно обернуть его в одинарные кавычки
