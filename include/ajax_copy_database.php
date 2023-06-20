71419c3dfca68d7aeca36406c3b3abf2a1ea4a55
Rus1anLev <im.ruslan.lev@gmail.com>
2023-06-20 18:16:41 +0300

    local db configured

--- include/ajax_copy_database.php
+++ include/ajax_copy_database.php
@@ -12,7 +12,6 @@ Loader::includeModule($MODULE_ID);
 $tg = new TelegramBot();
 
 
-
 $config_db = Configuration::getValue('connections')['default'];
 $config_db['site_dir'] = "/home/bitrix"; //Пока хардкодом
 
@@ -30,8 +29,9 @@ if(!empty($step)){
     switch($step) {
         case 1:
             // Дампм бд с прода
-            shell_exec('cd '.$config_db['site_dir'].' && mysqldump -u'.$config_db['login'].' -p\''.$config_db['password'].'\' '.$config_db['database'].' > '.$config_db['database'].'.sql');
-            //$tgRes = $tg->sendMessage('Дамп prod бд создан');
+            $tgRes = $tg->sendMessage('Старт дампа бд');
+            shell_exec('cd '.$config_db['site_dir'].' && mysqldump -u '.$config_db['login'].' -p\''.$config_db['password'].'\' '.$config_db['database'].' > '.$config_db['database'].'.sql');
+            
             $result = array('success' => 'Y', 'message' => 'Дамп prod бд создан',  'sub_message' => '', 'tg' => $tgRes);
 
             break;
@@ -41,10 +41,10 @@ if(!empty($step)){
                 //shell_exec('rsync --rsh=\'ssh -p '.$arDefaultOptions['DEV_REMOTE_PORT'].'\' '.$config_db['site_dir'].'/'.$config_db['database'].'.sql '.$arDefaultOptions['DEV_REMOTE_HOST'].':'.$arDefaultOptions['DEV_SITE_DIR']);
                 //shell_exec('ssh -p '.$arDefaultOptions['DEV_REMOTE_PORT'].' '.$arDefaultOptions['DEV_REMOTE_HOST'].' "mysql -u '.$arDefaultOptions['DEV_DATABASE_LOGIN'].' -p'.$arDefaultOptions['DEV_DATABASE_PASSWORD'].' '.$arDefaultOptions['DEV_DATABASE_NAME'].' < '.$arDefaultOptions['DEV_SITE_DIR'].'/'.$config_db['database'].'.sql"');
             }else{
-                //shell_exec('mysql -u'.$arDefaultOptions['DEV_DATABASE_LOGIN'].' -p\''.$arDefaultOptions['DEV_DATABASE_PASSWORD'].'\' '.$arDefaultOptions['DEV_DATABASE_NAME'].' < '.$config_db['site_dir'].'/'.$config_db['database'].'.sql');
+                shell_exec('mysql -u '.$arDefaultOptions['DEV_DATABASE_LOGIN'].' -p'.$arDefaultOptions['DEV_DATABASE_PASSWORD'].' '.$arDefaultOptions['DEV_DATABASE_NAME'].' < '.$config_db['site_dir'].'/'.$config_db['database'].'.sql');
             }
-            //shell_exec('rm -f '.$config_db['site_dir'].'/'.$config_db['database'].'.sql');
-            //$tgRes = $tg->sendMessage('Prod бд залита на dev сайт');
+            shell_exec('rm -f '.$config_db['site_dir'].'/'.$config_db['database'].'.sql');
+            $tgRes = $tg->sendMessage('Prod бд залита на dev сайт');
             $result = array('success' => 'Y', 'message' => 'Prod бд залита на dev сайт',  'sub_message' => '', 'tg' => $tgRes);
 
             break;
